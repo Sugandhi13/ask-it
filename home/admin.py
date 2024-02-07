a2e0334005134c6c0fa9468ba4db13fbab3d6840
Sugandhi13 <>
2024-02-07 12:47:07 +0000

    add Ask and Reply models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Ask, Reply
 
 # Register your models here.
+admin.site.register(Ask)
+admin.site.register(Reply)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Ask(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="ask_it"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+class Reply(models.Model):
+    ask = models.ForeignKey(
+        Ask, on_delete=models.CASCADE, related_name="reply_of"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="replied_by"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
