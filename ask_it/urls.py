60baac8c0ba8019abb59184d682af4b22ae3affa
Sugandhi13 <>
2024-02-07 13:13:23 +0000

    add basic template to test home page

--- ask_it/urls.py
+++ ask_it/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home import views as home_views
 
 urlpatterns = [
+    path("", include("home.urls"), name="home-urls"),
     path('admin/', admin.site.urls),
-    path('', home_views.index, name='index'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Ask
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World!")
+class AskitList(generic.ListView):
+    model = Ask
